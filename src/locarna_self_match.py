8ec28484d53c791e7a15c2daa98c305600de7e58
Raymond DeRoo <rderoo@Raymonds-MacBook-Pro.local>
2020-12-11 18:07:30 -0700

    Pre generate RS values

--- src/locarna_self_match.py
+++ src/locarna_self_match.py
@@ -5,30 +5,48 @@ Created on Wed Oct 14 20:05:50 2020
 @author: willi
 """
 
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import sys
 import subprocess
 import os
 import time
-import pickle
 
-utr_dir = '../data/utr_negative_aln/lstm100/'
+from os import path
+from csv import reader
 
-rs_dir = '../data/utr_negative_aln/eukaryotic_RS_fastas/'
-save_dir = '../data/utr_negative_aln/utr_aln_combined/'
+utr_dir         = '../data/utr_negative_aln/lstm100/'
 
+rs_dir          = '../data/utr_negative_aln/eukaryotic_RS_fastas/'
+save_dir        = '../data/utr_negative_aln/utr_aln_combined/'
 
+rs_self_file    = rs_dir + 'self_compare_list.txt'
+rs_list         = {}
 
 def align(file1, file2, e):
-    aln = subprocess.check_output(('locarna ' + file1 + ' ' + file2 + ' --struct-local=1'), shell=True, stderr=e)
+    aln = subprocess.check_output((f'locarna --struct-local=1 {file1} {file2}'), shell=True, stderr=e)
     aln_score = int(aln.decode('utf-8').split('\n')[0].split(' ')[1])
     return aln_score
 
 
+def load_rs_values(rs_file):
+    with open(rd_dir + rs_self_file, 'r') as fh:
+        csv_reader = reader(fh)
+        for line in csv_reader:
+            rs_list[line[0]] = line[1]
+    return rs_list
 
-rs_files = os.listdir(rs_dir)
+
+if os.is_file(rd_dir + rs_self_file):
+    rs_list = load_rs_values(rd_dir + rs_self_file)
+else: 
+    rs_files = os.listdir(rs_dir)
+    e = open('err.txt', 'w')
+    with open(rd_dir + rs_self_file, 'w') as fh:
+    for rs_file in rs_files:
+        token = rs_file.split('_')[1]
+        ref_score = align(rs_dir + rs_file,rs_dir + rs_file, e) # can we move this to outside the j loop and into the i loop,
+        fh.write(f'{token},{ref_score}')
+    rs_list = load_rs_values(rd_dir + rs_self_file)
+
 
 utr_names = [x.split('.')[0].split('_')[-1] for x in utr_files]
 
@@ -42,7 +60,7 @@ for i in range(len(utr_files)):
 
     utr = utr_files[i]
     utr_name = utr_names[i]
-    e = open('err.txt', 'w')
+    e = open('err.txt', 'a')
 
     print('-------------------')
     print('aligning: %s' % utr)
@@ -50,18 +68,18 @@ for i in range(len(utr_files)):
 
     st = time.time()
 
-    alignment_scores = np.zeros((len(rs_files), 3))
+    alignment_scores = np.zeros((len(rs_list), 3))
     alignment_names = []
     start = time.time()
 
-    for j in range(len(rs_files)):
+    for j in range(len(rs_list)):
 
         if j % 5 == 0:
             diff = time.time() - start
             print('I have completed', j, 'alns, it has taken', diff, 'to do alns', (j - 5), 'thru', j)
             start = time.time()
 
-        rs = rs_files[j]
+        rs = rs_list[j]
 
         # ref_score = align(rs_dir + rs,rs_dir+rs,e) # can we move this to outside the j loop and into the i loop,
         # or completely separate program that calculates and then fetch in loop
